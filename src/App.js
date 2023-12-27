73de6c104eda2bc997c9cf1808b512aa3b7c03eb
Mr-Shayan <shayanshaikh999@gmail.com>
2023-12-27 23:09:31 +0530

    success new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import {Login} from "./Components/Login";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./Components/login";
 import Contact from "./Components/Contact";
 import Prac from "./Components/Prac";
