9dbecde9eb6981c4616c268fc1d069061ce2c052
Dipak Wani <dipakwani204@gmail.com>
2023-12-28 04:31:48 +0530

    git commit -m "conflicts"

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Dashboard from './Components/Dashboard';
 import { StudentSidebar } from './Components/StudentSidebar';
 import { AdminSidebar } from './Components/AdminSidebar';
 import { CompanySidebar } from './Components/CompanySidebar';
+import { StudentRegister } from './Components/studentRegister';
+import { CompanyRegister } from './Components/companyRegister';
+import StudentInfo from './Components/StudentInfo';
+import { UpdateCompanyProfile } from './Components/UpdateCompanyProfile';
 
 
 
@@ -16,17 +20,23 @@ function App() {
   return (
 
     <BrowserRouter>
-     <NavigationBar></NavigationBar>
-    <Routes>
-      <Route path='/' element={<Login><Footer></Footer></Login>}></Route> 
-      <Route path="/dashboard/:userType" element={<Dashboard/>}></Route>
-      <Route path='/s_sidebar' element={<StudentSidebar></StudentSidebar>}></Route> 
-      <Route path='/a_sidebar' element={<AdminSidebar></AdminSidebar>}></Route> 
-      <Route path='/c_sidebar' element={<CompanySidebar></CompanySidebar>}></Route> 
-    </Routes>
-    <Footer></Footer>
+      <NavigationBar></NavigationBar>
+      <Routes>
+        <Route path='/' element={<Login><Footer></Footer></Login>}></Route>
+        <Route path="/dashboard/:userType" element={<Dashboard />}></Route>
+        <Route path='/s_sidebar' element={<StudentSidebar></StudentSidebar>}></Route>
+        <Route path='/a_sidebar' element={<AdminSidebar></AdminSidebar>}></Route>
+        <Route path='/c_sidebar' element={<CompanySidebar></CompanySidebar>}></Route>
+        <Route path="/" element={<Login></Login>}></Route>
+        <Route path="/student-register" element={<StudentRegister></StudentRegister>}/>
+        <Route path="/company-register" element={<CompanyRegister></CompanyRegister>}></Route>
+        <Route path="/student-update" element={<StudentInfo></StudentInfo>}></Route>
+        <Route path="/AddNewJob" element={<AddNewJob></AddNewJob>}></Route>
+        <Route path="/UpdateCompanyDetail" element={<UpdateCompanyProfile></UpdateCompanyProfile>}></Route>
+      </Routes>
+      <Footer></Footer>
     </BrowserRouter>
-    
+
   );
 }
 
