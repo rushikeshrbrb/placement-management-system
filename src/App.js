e1b595aaaf81319dd16438cb1696caf0c98ea213
sushant <borkar.sushant3@gmail.com>
2023-12-27 19:06:53 +0530

    added company registration form and student registration form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 // import {Login} from "./Components/Login";
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Login } from './Components/login';
-
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./Components/login";
+import { StudentRegister } from "./Components/studentRegister";
+import { CompanyRegister } from "./Components/companyDetails";
 
 function App() {
   return (
     <BrowserRouter>
-     
-    <Routes>
-      <Route path='/' element={<Login></Login>}></Route>  
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login></Login>}></Route>
+        <Route
+          path="/student-register"
+          element={<StudentRegister></StudentRegister>}
+        ></Route>
+        <Route
+          path="/company-register"
+          element={<CompanyRegister></CompanyRegister>}
+        ></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
