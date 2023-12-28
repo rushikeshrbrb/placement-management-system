d5715dca2d0818489d3318717e58353ea5899343
Anisha-1304 <anishavanjari1307@gmail.com>
2023-12-28 18:07:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-// import {Login} from "./Components/Login";
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Login } from './Components/login';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { StudentRegister } from "./Components/studentRegister";
+import { CompanyRegister } from "./Components/companyDetails";
 
+import { AddNewJob } from "./Components/AddNewJob";
+import { UpdateCompanyProfile } from "./Components/UpdateCompanyProfile";
+import Login from "./Components/login";
+import { StudentInfo } from "./Components/StudentInfo";
 
 function App() {
   return (
     <BrowserRouter>
-     
-    <Routes>
-      <Route path='/' element={<Login></Login>}></Route>  
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login></Login>}></Route>
+        <Route
+          path="/student-register"
+          element={<StudentRegister></StudentRegister>}
+        />
+        <Route
+          path="/company-register"
+          element={<CompanyRegister></CompanyRegister>}
+        ></Route>
+        <Route
+          path="/student-update"
+          element={<StudentInfo></StudentInfo>}
+        ></Route>
+        <Route path="/AddNewJob" element={<AddNewJob></AddNewJob>}></Route>
+        <Route
+          path="/UpdateCompanyDetail"
+          element={<UpdateCompanyProfile></UpdateCompanyProfile>}
+        ></Route>
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
