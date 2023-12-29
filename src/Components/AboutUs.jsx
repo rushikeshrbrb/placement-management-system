3d8a28286e4f6dc0926da6a14f3863aea0ab5cad
Dipak Wani <dipakwani204@gmail.com>
2023-12-29 22:27:52 +0530

    about us and merging

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -3,43 +3,70 @@ import { Alert, Button, Card, Col, Container, Row } from "react-bootstrap";
 import Dipak from "../Images/dipak.jpg";
 import Sushant from "../Images/sushant.png";
 import Anisha from "../Images/anisha.jpg";
-
-
-
+import Rushikesh from '../Images/rushikesh.jpg';
+import Shayan from '../Images/shayan.jpg';
+import './Aboutus.css';
 
 export default function AboutUs() {
     return (
         <Container fluid>
-            <Alert
-                variant="success"
-                style={{
-                    textAlign: "center",
-                    backgroundColor: "#888888",
-                    color: "white",
-                }}
-            >
-                <h1>About Us</h1>
-            </Alert>
+            <br /><br /> <br />
+            <Container>
+            <Row className="justify-content-md-center">
+                <Col  md={8}>
+                        <Card style={{width:"800px"}} >
+                            <Card.Body>
+                                <Card.Title style={{ color: 'black', fontWeight: 'bold', textAlign: 'center' }}>Purpose of the Training & Placement Cell</Card.Title>
+                                <Card.Text style={{ color: 'black', fontStyle: 'italic', textAlign: 'center' }}>
+                                    The Training & Placement Cell at our institution is a dynamic and integral part of our commitment to shaping the future of our students. Our primary objective is to bridge the gap between academia and industry by facilitating meaningful interactions and collaborations.
+
+                                      <br />Talk with Expert's
+                                      <br /> Industry Interaction
+                                    <br />  Campus Recruitment Drives
+                                    <br />  Individual Career Counseling
+                                  <br />  Skill Enhancement Initiatives
+                                    <br />  Preparing Industry-Ready Graduates
+                                    
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+
+                    </Col>
+                </Row>
+            </Container>
+
             <Row>
                 <Col lg={4}>
-                    <h3 style={{ color: 'black', fontWeight: 'bold' }}>Vision</h3>
-                    <p style={{ color: 'black', fontStyle: 'italic' }}>
-                        Go-Fit is a health and fitness company offering digital and offline experiences across fitness, nutrition, and mental well-being.
-                        With the aim to make fitness fun and easy, and medical and lifestyle care hassle-free.
-                    </p>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title style={{ color: 'black', fontWeight: 'bold' }}>Vision</Card.Title>
+                            <Card.Text style={{ color: 'black', fontStyle: 'italic' }}>
+                                Equipping the students with relevant and conceptualized professional skills and guiding them towards a bright future and career all around the world with the values of Sincerity, Hard Work, and Justice. The Cell endeavors to carry out successfully all the processes methodically throughout the year.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Col>
                 <Col lg={4}>
-                    <h3 style={{ color: 'black', fontWeight: 'bold' }}>Mission</h3>
-                    <p style={{ color: 'black', fontStyle: 'italic' }}>Go-fit's mission is to “make health easy”.
-
-                        Through our products and services, we want to enable people to significantly improve their overall health, reduce the risk of lifestyle diseases and enable a long, disease free life.</p>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title style={{ color: 'black', fontWeight: 'bold' }}>Mission</Card.Title>
+                            <Card.Text style={{ color: 'black', fontStyle: 'italic' }}>
+                                To achieve 100% placement for students through dedication, attitude, and complete involvement is our mission. The Training and Placement Cell, guided by a set of rules and principles, strives to maintain a good relationship with industries. Preparing the recruitment schedule for the year, inviting corporations for pre-placement.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Col>
                 <Col lg={4}>
-                    <h3 style={{ color: 'black', fontWeight: 'bold' }}>Our History</h3>
-                    <p style={{ color: 'black', fontStyle: 'italic' }}>Co-founded by Anuja Gupta, Yogesh patel and Dipak Wani in CDAC -Mumbai 2023, the company is headquartered in Kharghar, Mumbai.</p>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title style={{ color: 'black', fontWeight: 'bold' }}>Our Objective</Card.Title>
+                            <Card.Text style={{ color: 'black', fontStyle: 'italic' }}>
+                                To provide class training for the students through a continuous training module. To enhance the employability skills among the students to meet corporate expectations. To produce the most competitive students to fit in all scenarios of the job market. To place all the students in prospective IT, Management Companies.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
-
 
             <Alert
                 variant="danger"
@@ -49,94 +76,96 @@ export default function AboutUs() {
                 }}
             > <h1>Meet Our Team </h1>
             </Alert>
-<center>
+            <center>
 
 
-            <Row className="mx-5" >
-                <Col lg={4} style={{ textAlign: "center" }}>
-                    <Card style={{ width: '25rem' }}>
-                        <Card.Img variant="top" src={Dipak} />
-                        <Card.Body>
-                            <Card.Title>Dipak Wani</Card.Title>
-                            <Card.Text>
-                            PRN : 230940520019 <br />
-                                PG-DAC Student, Sep-23 batch <br />
-                                CDAC Mumbai JH 
-                            </Card.Text>
-                            <Button variant="info"><a href="https://www.linkedin.com/in/dipak-wani-726b2521a/">LinkedIn</a></Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg={4} style={{ textAlign: "center" }}>
+                <Row className="mx-5" >
+                    <Col lg={4} style={{ textAlign: "center" }}>
+                        <Card style={{ width: '20rem' }}>
+                            <Card.Img variant="top" src={Dipak} />
+                            <Card.Body>
+                                <Card.Title><b><u>Dipak Wani</u></b> </Card.Title>
+                                <Card.Text>
+                                    PRN : 230940520019 <br />
+                                    PG-DAC Student, Sep-23 batch <br />
+                                    CDAC Mumbai JH
+                                </Card.Text>
+                                <Button variant="info"><a href="https://www.linkedin.com/in/dipak-wani-726b2521a/">LinkedIn</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={4} style={{ textAlign: "center" }}>
 
-                <Card style={{ width: '25rem', transition: 'transform 0.3s', ':hover': { transform: 'scale(1.05)' } }}>
-                        <Card.Img variant="top" src={Sushant} />
-                        <Card.Body>
-                            <Card.Title>Anuja Gupta</Card.Title>
-                            <Card.Text>
-                                PRN : 230940320022 <br />
-                                PG-DAC Student, Sep-23 batch <br />
-                                CDAC Mumbai KH 
-                            </Card.Text>
-                            <Button variant="info"><a href="https://www.linkedin.com/in/anuja-gupta-7b89ba148/">LinkedIn</a></Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg={4} style={{ textAlign: "center" }}>
-                    <Card style={{ width: '25rem' }}>
-                        <Card.Img variant="top" src={Anisha} />
-                        <Card.Body>
-                            <Card.Title>Yogesh Patel</Card.Title>
-                            <Card.Text>
-                            PRN : 230940320132 <br />
-                                PG-DAC Student, Sep-23 batch <br />
-                                CDAC Mumbai KH 
-                            </Card.Text>
-                            <Button variant="info"><a href="https://www.linkedin.com/in/yogesh-patel-8b426b229/">LinkedIn</a></Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <hr />
-            <Row >
-            <Col lg={2} style={{ textAlign: "center" }}>
-                </Col>
-                <Col lg={4} style={{ textAlign: "center" }}>
-                    <Card style={{ width: '25rem' }}>
-                        <Card.Img variant="top" src={Dipak} />
-                        <Card.Body>
-                            <Card.Title>Dipak Wani</Card.Title>
-                            <Card.Text>
-                            PRN : 230940520019 <br />
-                                PG-DAC Student, Sep-23 batch <br />
-                                CDAC Mumbai JH 
-                            </Card.Text>
-                            <Button variant="info"><a href="https://www.linkedin.com/in/dipak-wani-726b2521a/">LinkedIn</a></Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg={4} style={{ textAlign: "center" }}>
+                        <Card style={{ width: '20rem', transition: 'transform 0.3s', ':hover': { transform: 'scale(1.05)' } }}>
+                            <Card.Img variant="top" src={Sushant} />
+                            <Card.Body>
+                                <Card.Title><b><u>Shushant Borkar</u></b></Card.Title>
+                                <Card.Text>
+                                    PRN : 230940520089 <br />
+                                    PG-DAC Student, Sep-23 batch <br />
+                                    CDAC Mumbai JH
+                                </Card.Text>
+                                <Button variant="info"><a href="https://www.linkedin.com/in/sushant-borkar-10bb4718b/">LinkedIn</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={4} style={{ textAlign: "center" }}>
+                        <Card style={{ width: '20rem' }}>
+                            <Card.Img variant="top" src={Rushikesh} />
+                            <Card.Body>
+                                <Card.Title><b><u>Rushikesh Baravkar</u></b></Card.Title>
+                                <Card.Text>
+                                    PRN : 230940520068 <br />
+                                    PG-DAC Student, Sep-23 batch <br />
+                                    CDAC Mumbai JH
+                                </Card.Text>
+                                <Button variant="info"><a href="https://www.linkedin.com/in/rushikesh-baravkar-a0a384224/">LinkedIn</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <hr />
+                <Row >
+                    <Col lg={2} style={{ textAlign: "center" }}>
+                    </Col>
+                    <Col lg={4} style={{ textAlign: "center" }}>
+                        <Card style={{ width: '20rem' }}>
+                            <Card.Img variant="top" src={Anisha} />
+                            <Card.Body>
+                                <Card.Title><b><u>Anisha Vanjari</u></b></Card.Title>
+                                <Card.Text>
+                                    PRN : 230940520019 <br />
+                                    PG-DAC Student, Sep-23 batch <br />
+                                    CDAC Mumbai KH
+                                </Card.Text>
+                                <Button variant="info"><a href="https://www.linkedin.com/in/anisha-vanjari-b875a4244/">LinkedIn</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={4} style={{ textAlign: "center" }}>
 
-                <Card style={{ width: '25rem', transition: 'transform 0.3s', ':hover': { transform: 'scale(1.05)' } }}>
-                        <Card.Img variant="top" src={Sushant} />
-                        <Card.Body>
-                            <Card.Title>Anuja Gupta</Card.Title>
-                            <Card.Text>
-                                PRN : 230940320022 <br />
-                                PG-DAC Student, Sep-23 batch <br />
-                                CDAC Mumbai KH 
-                            </Card.Text>
-                            <Button variant="info"><a href="https://www.linkedin.com/in/anuja-gupta-7b89ba148/">LinkedIn</a></Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg={2} style={{ textAlign: "center" }}>
-                </Col>
-            </Row>
-           
+                        <Card style={{ width: '20rem', transition: 'transform 0.3s', ':hover': { transform: 'scale(1.05)' } }}>
+                            <Card.Img variant="top" src={Shayan} />
+                            <Card.Body>
+                                <Card.Title><b><u>Shayan Shaikh</u></b></Card.Title>
+                                <Card.Text>
+                                    PRN : 230940520017 <br />
+                                    PG-DAC Student, Sep-23 batch <br />
+                                    CDAC Mumbai KH
+                                </Card.Text>
+                                <Button variant="info"><a href="https://www.linkedin.com/in/shayanshaikh999/">LinkedIn</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={2} style={{ textAlign: "center" }}>
+                    </Col>
+                </Row>
+
             </center>
+
+
             <hr />
-            
+
             <Row>
                 <Col>
                     <Card>
@@ -159,10 +188,10 @@ export default function AboutUs() {
                         <Card.Body>
                             <blockquote className="blockquote mb-5 ">
                                 <p>
-                                Teamwork is the secret that makes common people achieve uncommon results. - Ifeanyi Enoch Onuoha
+                                    Teamwork is the secret that makes common people achieve uncommon results. - Ifeanyi Enoch Onuoha
                                 </p>
                                 <footer className="blockquote-footer">
-                                Helen Keller <cite title="Source Title"> </cite>
+                                    Helen Keller <cite title="Source Title"> </cite>
                                 </footer>
                             </blockquote>
                         </Card.Body>
@@ -174,10 +203,10 @@ export default function AboutUs() {
                         <Card.Body>
                             <blockquote className="blockquote mb-5 ">
                                 <p>
-                                Teamwork is the secret that makes common people achieve uncommon results. 
+                                    Teamwork is the secret that makes common people achieve uncommon results.
                                 </p>
                                 <footer className="blockquote-footer">
-                                - Ifeanyi Enoch Onuoha<cite title="Source Title"> </cite>
+                                    - Ifeanyi Enoch Onuoha<cite title="Source Title"> </cite>
                                 </footer>
                             </blockquote>
                         </Card.Body>
@@ -186,8 +215,8 @@ export default function AboutUs() {
             </Row>
 
 
-                    
-      
+
+
         </Container>
     );
 }
